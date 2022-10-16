fc88645bde933f22044d6329f04a091ee8817808
Gabriel Borges <gabrielborges565@gmail.com>
2022-10-16 00:47:10 -0300

    Configuracao do Banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host:  process.env.DB_HOST,
+      port: 3306,
+      username: 'jackson',
+      password: '!123ABCabc',
+      database: 'timeto',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
